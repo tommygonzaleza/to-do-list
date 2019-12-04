95ec7f499472bd0746df249d187070c662812992
tommygonzaleza <tommygonzaleza@users.noreply.github.com>
2019-12-04 18:03:27 -0400

    Styles added to the boilerplate

--- to-do-list/src/js/component/home.js
+++ to-do-list/src/js/component/home.js
@@ -6,25 +6,23 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="container text-center">
-			<div>
-				<header className="entire-header row col-8 justify-content-center">
-					<h1>Task List</h1>
-				</header>
-				<section className="entire-body row col-8 justify-content-center">
-					<form className="form-styles">
-						<input placeholder="New tasks..." />
-					</form>
-					<ul className="main-list ul-style">
-						<li className="list-item">Task 1</li>
-						<li className="list-item">Task 2</li>
-						<li className="list-item">Task 3</li>
-					</ul>
-				</section>
-				<footer className="list-footer row">
-					<p>x tasks left.</p>
-				</footer>
-			</div>
+		<div className="box_1 container">
+			<header className="entire-header col-12 row justify-content-center">
+				<h1>Task List</h1>
+			</header>
+			<section className="entire-body row justify-content-center">
+				<form className="form-style">
+					<input placeholder="New tasks..." />
+				</form>
+				<ul className="main-list ul-style">
+					<li className="list-item">Take a Shower</li>
+					<li className="list-item">Wash the Dishes</li>
+					<li className="list-item">Cook Dinner</li>
+				</ul>
+				<div className="task-left-style">
+					<p>X tasks left</p>
+				</div>
+			</section>
 		</div>
 	);
 }
