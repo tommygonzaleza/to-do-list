d9ce8f466b941150345b83415c4c0008f32f0fe7
tommygonzaleza <tommygonzaleza@users.noreply.github.com>
2020-01-24 13:19:16 -0400

    Add new tasks with fetch done

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -67,14 +67,61 @@ export class Home extends React.Component {
 	}
 	handleAddTasks(e) {
 		e.preventDefault();
+		let currentTasks = this.state.tasks;
+		currentTasks.push({
+			label: this.state.newTask,
+			done: false
+		})
 		this.setState({
-			tasks: [...this.state.tasks, this.state.newTask],
+			...this.state,
 			newTask: ""
 		});
+		console.log(currentTasks);
+		fetch(
+			"https://assets.breatheco.d/apis/fake/todos/user/tommygonzaleza",
+			{
+				method: "PUT",
+				body: JSON.stringify(currentTasks),
+				headers: {
+					"Content-type": "application/json"
+				}
+			}
+		)
+			.then(res => {
+				if (res.ok) {
+					fetch(
+						"https://assets.breatheco.d/apis/fake/todos/user/tommygonzaleza",
+						{
+							method: "GET",
+							headers: {
+								"Content-type": "application/json"
+							}
+						}
+					)
+						.then(res => {
+							return Response.json();
+						})
+						.then(data => {
+							console.log(data);
+							this.setState({
+								tasks: data,
+								newTask: ""
+							});
+						})
+						.catch(error => {
+							console.log(error);
+						});
+				} else {
+					console.log("Ocurred an  error fetching tasks ", res.text());
+				}
+			})
+			.catch(error => {
+				console.log(error);
+			});
 	}
 	handleInputChange(e) {
 		this.setState({
-			...this.state.tasks,
+			...this.state,
 			newTask: e.target.value
 		});
 	}
