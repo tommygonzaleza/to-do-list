69441c17417ac8eb2f21972dfe9bd1f107359d7b
tommygonzaleza <tommygonzaleza@users.noreply.github.com>
2020-01-28 17:56:24 -0400

    Delete Tasks Fetch Doesn't Function

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,7 +1,4 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import checkButton from "../../img/green-check.png";
 
 //create your first component
@@ -9,7 +6,7 @@ export class Home extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			tasks: [],
+			tasks: ["Sample Task"],
 			newTask: ""
 		};
 		this.handleAddTasks = this.handleAddTasks.bind(this);
@@ -27,10 +24,10 @@ export class Home extends React.Component {
 			}
 		)
 			.then(res => {
-				console.log(res.ok);
-				console.log(res.status);
-				console.log(res.text());
-				if (res.status == 404) {
+				let res1 = res.clone();
+				console.log(res1.ok);
+				console.log(res1.status);
+				if (res1.status == 404) {
 					let emptyArray = [];
 					fetch(
 						"https://assets.breatheco.de/apis/fake/todos/user/tommygonzaleza",
@@ -42,28 +39,37 @@ export class Home extends React.Component {
 							}
 						}
 					)
-						.then(res => {
+						.then(res2 => {
 							console.log("Tried to create user: ");
-							console.log(res.status);
-							console.log(res.text());
+							console.log(res2.status);
+							console.log(res2.text());
+							fetch(
+								"https://assets.breatheco.de/apis/fake/todos/user/tommygonzaleza",
+								{
+									method: "GET",
+									headers: {
+										"Content-type": "application/json"
+									}
+								}
+							).then(res3 => res3.json());
 						})
 						.catch(error => {
 							console.log(error);
 						});
 				} else {
-					return Response.json();
+					return res1.json();
 				}
 			})
-				.then(data => {
-					console.log(data);
-					this.setState({
-						tasks: data,
-						newTask: ""
-					})
-				})
-				.catch(error => {
-					console.log(error)
+			.then(data => {
+				console.log(data);
+				this.setState({
+					tasks: data,
+					newTask: ""
 				});
+			})
+			.catch(error => {
+				console.log(error);
+			});
 	}
 	handleAddTasks(e) {
 		e.preventDefault();
@@ -71,14 +77,14 @@ export class Home extends React.Component {
 		currentTasks.push({
 			label: this.state.newTask,
 			done: false
-		})
+		});
 		this.setState({
 			...this.state,
 			newTask: ""
 		});
 		console.log(currentTasks);
 		fetch(
-			"https://assets.breatheco.d/apis/fake/todos/user/tommygonzaleza",
+			"https://assets.breatheco.de/apis/fake/todos/user/tommygonzaleza",
 			{
 				method: "PUT",
 				body: JSON.stringify(currentTasks),
@@ -87,10 +93,10 @@ export class Home extends React.Component {
 				}
 			}
 		)
-			.then(res => {
-				if (res.ok) {
+			.then(res1 => {
+				if (res1.ok) {
 					fetch(
-						"https://assets.breatheco.d/apis/fake/todos/user/tommygonzaleza",
+						"https://assets.breatheco.de/apis/fake/todos/user/tommygonzaleza",
 						{
 							method: "GET",
 							headers: {
@@ -98,8 +104,8 @@ export class Home extends React.Component {
 							}
 						}
 					)
-						.then(res => {
-							return Response.json();
+						.then(res2 => {
+							return res2.json();
 						})
 						.then(data => {
 							console.log(data);
@@ -112,7 +118,7 @@ export class Home extends React.Component {
 							console.log(error);
 						});
 				} else {
-					console.log("Ocurred an  error fetching tasks ", res.text());
+					console.log("Error fetching tasks ", res1.text());
 				}
 			})
 			.catch(error => {
@@ -125,14 +131,51 @@ export class Home extends React.Component {
 			newTask: e.target.value
 		});
 	}
-	handleDeleteTask(e, indexToDelete) {
+	handleDeleteTask(indexToDelete) {
 		let tasksLeft = this.state.tasks.filter(
-			(value, index) => index != indexToDelete
+			index => index != indexToDelete
 		);
-		this.setState({
-			tasks: tasksLeft,
-			newTask: this.state.newTask
-		});
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/tommygonzaleza",
+			{
+				method: "PUT",
+				body: JSON.stringify(tasksLeft),
+				headers: {
+					"Content-type": "application/json"
+				}
+			}
+		)
+			.then(res1 => {
+				if (res1.ok) {
+					fetch(
+						"https://assets.breatheco.de/apis/fake/todos/user/tommygonzaleza",
+						{
+							method: "GET",
+							headers: {
+								"Content-type": "application/json"
+							}
+						}
+					)
+						.then(res2 => {
+							return res2.json();
+						})
+						.then(data => {
+							console.log(data);
+							this.setState({
+								tasks: data,
+								newTask: ""
+							});
+						})
+						.catch(error => {
+							console.log(error);
+						});
+				} else {
+					console.log("Error fetching tasks ", res1.text());
+				}
+			})
+			.catch(error => {
+				console.log(error);
+			});
 	}
 	render() {
 		let tasks = this.state.tasks;
@@ -152,14 +195,14 @@ export class Home extends React.Component {
 						/>
 					</form>
 					<ul className="main-list ul-style mx-auto my-auto">
-						{tasks.map((value, index) => {
+						{this.state.tasks.map((value, index) => {
 							return (
 								<li key={index} className="li-style col-12">
-									{value}
+									{value.label}
 									<img
 										className="check-style"
 										onClick={e =>
-											this.handleDeleteTask(e, index)
+											this.handleDeleteTask(index)
 										}
 										src={checkButton}
 									/>
